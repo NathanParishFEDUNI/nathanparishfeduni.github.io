14199b155afec21a054c73f4974c80a4ade3ae4a
NathanParishFEDUNI <nathanparish@students.federation.edu.au>
2026-09-03 10:50:42 +1000

    Attribution

--- app.js
+++ app.js
@@ -25,10 +25,6 @@ const map = L.map("map", {
 
 L.tileLayer(
     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    {
-        attribution:
-            "OpenStreetMap | Leaflet     "
-    }
 ).addTo(map);
 
 
